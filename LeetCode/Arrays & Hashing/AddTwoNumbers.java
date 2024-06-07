e35ed804db737196fc2686f0dc5ce1a407e664a8
Summer079 <123596261+Summer079@users.noreply.github.com>
2024-06-06 23:24:34 -0500

    Update AddTwoNumbers.java
    Full Description

--- LeetCode/Arrays & Hashing/AddTwoNumbers.java
+++ LeetCode/Arrays & Hashing/AddTwoNumbers.java
@@ -1,3 +1,12 @@
+package All_File;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
@@ -21,5 +30,27 @@ class Solution {
         }
 
         return dummyHead.next;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Example usage
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        Solution solution = new Solution();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+       
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
     }
 }
