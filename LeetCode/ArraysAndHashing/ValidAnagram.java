b78e1846ae4cf73bdd79060e2f442ee74ebdb92a
Summer079 <123596261+Summer079@users.noreply.github.com>
2024-06-14 22:42:51 -0500

    Update ValidAnagram.java

--- LeetCode/ArraysAndHashing/ValidAnagram.java
+++ LeetCode/ArraysAndHashing/ValidAnagram.java
@@ -1,3 +1,12 @@
+/**
+* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+*
+* An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters
+* exactly once.
+*/
+
+
+
 package ArraysAndHashing;
 
 public class ValidAnagram {
