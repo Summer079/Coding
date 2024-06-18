92ca97b3f2cd707ea3545f554625a712bc4b427c
Summer079 <123596261+Summer079@users.noreply.github.com>
2024-06-17 19:49:38 -0500

    Update LongestConsecutiveSequence.java

--- LeetCode/ArraysAndHashing/LongestConsecutiveSequence.java
+++ LeetCode/ArraysAndHashing/LongestConsecutiveSequence.java
@@ -1,3 +1,27 @@
+/**
+ * Longest Consecutive Sequence
+
+Given an array of integers nums, return the length of the longest consecutive sequence of elements.
+
+A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element.
+
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+
+Input: nums = [2,20,4,10,3,4,5]
+
+Output: 4
+
+Explanation: The longest consecutive sequence is [2, 3, 4, 5].
+
+Example 2:
+
+Input: nums = [0,3,2,5,4,6,1,1]
+
+Output: 7
+ */
+
 package ArraysAndHashing;
 
 import java.util.*;
