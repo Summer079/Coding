9af6da1c307b93f2f5cf376470669d2ff7b89bb2
Summer079 <123596261+Summer079@users.noreply.github.com>
2024-06-19 21:16:41 -0500

    Update ReverseLinkedList.java

--- LeetCode/LinkedList/ReverseLinkedList.java
+++ LeetCode/LinkedList/ReverseLinkedList.java
@@ -1,6 +1,22 @@
+/**
+ * Reverse a Linked List
+ *
+ * Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
+ *
+ * Example 1:
+ *
+ * Input: head = [0,1,2,3]
+ * Output: [3,2,1,0]
+ * 
+ * Example 2:
+ *
+ * Input: head = []
+ * Output: []
+ * 
+ */
+
+
 package LinkedList;
-
-
 
 class ListNodes {
     int val;
